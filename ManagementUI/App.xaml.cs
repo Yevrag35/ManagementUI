e91fd2eca92d8e5740c2e827065e6c311bac58d7
Mike Garvey <mike.garvey@yevrag35.com>
2019-09-25 15:18:32 -0400

    Add project files.

---
+++ ManagementUI/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ManagementPicker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var main = new ManagementUI();
+            main.Show();
+        }
+    }
+}
