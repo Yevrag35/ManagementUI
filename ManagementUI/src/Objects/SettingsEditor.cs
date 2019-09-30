6ee2f497e616b65d358fe8ab00bfb8fb4590fdbf
Mike Garvey <mike.garvey@yevrag35.com>
2019-09-30 10:56:48 -0500

    SettingsEditor

---
+++ ManagementUI/src/Objects/SettingsEditor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagementUI
+{
+    public class SettingsEditor
+    {
+        private const string NOTEPAD = "{0}\\Notepad.exe";
+        private const string NOTEPAD_PP = "{0}\\Notepad++\\notepad++.exe";
+        private const string VS_CODE = "{0}\\Microsoft VS Code\\Code.exe";
+
+
+    }
+}
