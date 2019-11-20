744a6119e2b88912b12665659cb6fb71eb45e8f4
Mike Garvey <mike.garvey@yevrag35.com>
2019-11-19 22:35:54 -0500

    Need IComparable

--- ManagementUI/src/Objects/Settings/SettingsJson.cs
+++ ManagementUI/src/Objects/Settings/SettingsJson.cs
@@ -30,7 +30,14 @@ namespace ManagementUI
 
         #region CONSTRUCTORS
         public SettingsJson() : base() { }
-        public SettingsJson(string filePath) : base(filePath) { }
+        public SettingsJson(string filePath) : base(filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                this.CreateAppFolders();
+                this.SaveDefault();
+            }
+        }
 
         #endregion
 
@@ -38,6 +45,86 @@ namespace ManagementUI
         private void Settings_Changed(object sender, NotifyCollectionChangedEventArgs e) => this.Save();
 
         public void Save(SettingChangedAction action = SettingChangedAction.Save)
+        {
+            this.Save(((IJsonSettings)this).SettingsAsJson, action);
+        }
+
+        #endregion
+
+        #region DEFAULTS
+        //private JsonSerializerSettings GetDefaultSerializerSettings()
+        //{
+        //    var settings = new JsonSerializerSettings
+        //    {
+        //        ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        //        DateFormatHandling = DateFormatHandling.IsoDateFormat,
+        //        DateTimeZoneHandling = DateTimeZoneHandling.Local,
+        //        DefaultValueHandling = DefaultValueHandling.Include,
+        //        FloatParseHandling = FloatParseHandling.Decimal,
+        //        Formatting = Formatting.Indented,
+        //        NullValueHandling = NullValueHandling.Include,
+        //        MissingMemberHandling = MissingMemberHandling.Ignore
+        //    };
+        //    settings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
+        //    return settings;
+        //}
+
+        private JObject GetDefaultSettings()
+        {
+            return new JObject
+            {
+                new JProperty("$schema", "https://json.yevrag35.com/uimanagement/v1.0.0/schema.json"),
+                new JProperty("version", "1.0"),
+                new JProperty("editor", SettingsLauncher.Notepad.ToString().ToLower()),
+                new JProperty("settings", new JObject
+                {
+                    new JProperty("apps", new JArray())
+                })
+            };
+        }
+
+        public void SaveDefault()
+        {
+            JObject defSets = this.GetDefaultSettings();
+            this.Save(defSets, SettingChangedAction.Save);
+        }
+
+        #endregion
+
+        #region BACKEND/PRIVATE METHODS
+        private void CreateAppFolders()
+        {
+            string dir1 = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            try
+            {
+                string dir2 = dir1 + "\\Mike Garvey";
+                string dir3 = dir2 + "\\ManagementUI";
+                Directory.CreateDirectory(dir1);
+                Directory.CreateDirectory(dir2);
+                Directory.CreateDirectory(dir3);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(string.Format(
+                    "An error occurred: {0}{1}{1}{2}{1}{1}{3}",
+                    e.GetType().FullName,
+                    Environment.NewLine,
+                    e.Message,
+                    string.Format("LOCALAPPDATA: {0}", dir1)),
+                    "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext ctx)
+        {
+            if (this.Settings != null && this.Settings.Apps != null)
+            {
+                this.Settings.Apps.CollectionChanged += this.Settings_Changed;
+            }
+        }
+
+        private void Save(JObject saveThis, SettingChangedAction action)
         {
             using (var streamWriter = new StreamWriter(this.FilePath))
             {
@@ -52,46 +139,11 @@ namespace ManagementUI
                     Indentation = 1
                 })
                 {
-                    ((IJsonSettings)this).SettingsAsJson.WriteTo(writer, new StringEnumConverter(new CamelCaseNamingStrategy()));
+                    saveThis.WriteTo(writer, new StringEnumConverter(new CamelCaseNamingStrategy()));
                 }
             }
 
             base.OnSettingsChanged(new SettingsChangedEventArgs(action));
-        }
-
-        #endregion
-
-        #region BACKEND/PRIVATE METHODS
-        //private static void CreateAppFolders()
-        //{
-        //    string dir1 = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-        //    try
-        //    {
-        //        string dir2 = dir1 + "\\Mike Garvey";
-        //        string dir3 = dir2 + "\\ManagementUI";
-        //        Directory.CreateDirectory(dir1);
-        //        Directory.CreateDirectory(dir2);
-        //        Directory.CreateDirectory(dir3);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        MessageBox.Show(string.Format(
-        //            "An error occurred: {0}{1}{1}{2}{1}{1}{3}",
-        //            e.GetType().FullName,
-        //            Environment.NewLine,
-        //            e.Message,
-        //            string.Format("LOCALAPPDATA: {0}", dir1)),            
-        //            "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
-        //    }
-        //}
-
-        [OnDeserialized]
-        private void OnDeserialized(StreamingContext ctx)
-        {
-            if (this.Settings != null && this.Settings.Apps != null)
-            {
-                this.Settings.Apps.CollectionChanged += this.Settings_Changed;
-            }
         }
 
         #endregion
