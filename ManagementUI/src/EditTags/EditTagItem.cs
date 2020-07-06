3e70de3f7d6a22e2d6efa1d79280b003fb59de51
Mike Garvey <mike.garvey@yevrag35.com>
2020-07-06 18:11:59 -0400

    Json Helpers and Equality

---
+++ ManagementUI/src/EditTags/EditTagItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManagementUI.Editing
+{
+    public struct EditTagItem
+    {
+        public bool IsChecked;
+        public EditingStatus Status;
+        public string Title;
+
+        public static explicit operator EditTagItem(FilterTag filTag)
+        {
+            return new EditTagItem
+            {
+                IsChecked = filTag.IsChecked,
+                Status = EditingStatus.Available,
+                Title = filTag.Tag
+            };
+        }
+        public static explicit operator FilterTag(EditTagItem editTagItem) => new FilterTag(editTagItem.Title, editTagItem.IsChecked);
+
+        public static List<FilterTag> Apply(ICollection<EditTagItem> these)
+        {
+            var list = new List<FilterTag>(these.Count);
+            foreach (FilterTag eti in these)
+            {
+                list.Add(eti);
+            }
+            return list;
+        }
+    }
+
+    public enum EditingStatus
+    {
+        Available,
+        Applied
+    }
+}
